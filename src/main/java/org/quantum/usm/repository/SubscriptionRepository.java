5d5efdbe1fdade57c9eeed0955f70e45b615daac
quantum <liberiamor@gmail.com>
2025-05-31 12:04:00 +0300

    add repositories for user and subscriptions

---
+++ src/main/java/org/quantum/usm/repository/SubscriptionRepository.java
@@ -0,0 +1,8 @@
+package org.quantum.usm.repository;
+
+import org.quantum.usm.entity.Subscription;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriptionRepository extends CrudRepository<Subscription, Integer> {
+
+}
---
+++ src/main/java/org/quantum/usm/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package org.quantum.usm.repository;
+
+import org.quantum.usm.entity.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+}
